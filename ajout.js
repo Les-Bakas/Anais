7fad5ee63dd6776b10c90e05ce39286f1178e187
Anaïs <anais.andre@aol.fr>
2021-12-02 20:51:04 +0100

    js marche

--- ajout.js
+++ ajout.js
@@ -1,4 +1,12 @@
 function afficher(){
 	let ajout = document.getElementById("ajout");
-	ajout.style.display="block"
+	ajout.style.display="block";
+}
+
+document.getElementById("fermer").onclick= fermer;
+
+function fermer(){
+	let fermer= document.getElementById("ajout");
+	fermer.style.display="none";
+
 }
